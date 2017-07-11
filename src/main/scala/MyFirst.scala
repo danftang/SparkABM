574883391bc40f42fe3cc38bd45d7a8befbb6526
Daniel Tang <danieltang@improbable.io>
2017-07-11 22:21:05 +0100

    added caching

--- src/main/scala/MyFirst.scala
+++ src/main/scala/MyFirst.scala
@@ -5,10 +5,20 @@ import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.SparkSession
 
 object MyFirst {
+
   def main(args: Array[String]): Unit = {
     Logger.getLogger("org").setLevel(Level.ERROR)
     val spark = SparkSession.builder().master("local").appName("My App").getOrCreate()
-    val result = MyABM.run()
+    var i = 1
+    var result = MyABM.createGraph(10)
+    val edges = result.graph.edges.map((tuple) => {tuple}).cache()
+    var vertices = result.graph.vertices.map((tuple) => {tuple}).cache()
+    for(i <- 1 to 100) {
+      result = result.run(-1, MyABM.step, MyABM.send)
+      vertices = result.graph.vertices.map((tuple) => {tuple}).cache()
+      result = new ABMGraph[Int](vertices, edges)
+      println("step "+i)
+    }
     result.graph.vertices.collect().foreach(println)
   }
 }
